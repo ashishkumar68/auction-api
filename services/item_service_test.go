a1702a063cd905c121c8899d4605efa8887a4cef
Ashish Kumar <ashish.k@mindfiresolutions.com>
2022-04-26 12:46:16 +0530

    Fixed broken tests.

--- services/item_service_test.go
+++ services/item_service_test.go
@@ -8,20 +8,27 @@ import (
 )
 
 func (suite *ServiceTestSuite) TestPlaceBidOnItem() {
+	suite.DB.Exec(`
+INSERT INTO users(id, uuid, created_at, updated_at, first_name, last_name, email, password, is_active) 
+VALUES (6, uuid_v4(), NOW(), NOW(), "John", "Doe", "johndoe25@abc.com", "$2a$10$3QxDjD1ylgPnRgQLhBrTaeqdsNaLxkk7gpdsFGUheGU2k.l.5OIf6", 1)
+`)
 	item := suite.repository.FindItemById(1)
 	assert.NotNil(suite.T(), item)
+	bidUser := suite.repository.FindUserById(6)
+	assert.NotNil(suite.T(), bidUser)
+	assert.False(suite.T(), item.IsOwner(*bidUser))
 
 	placeBidForm := forms.PlaceNewItemBidForm{
 		ItemId:        item.ID,
-		BidUserId:     suite.actionUser.ID,
+		BidUserId:     bidUser.ID,
 		BidValue:      12,
-		AuditableForm: forms.AuditableForm{ActionUser: suite.actionUser},
+		AuditableForm: forms.AuditableForm{ActionUser: bidUser},
 	}
 	newBid, err := suite.itemService.PlaceItemBid(context.TODO(), placeBidForm)
 	assert.Nil(suite.T(), err)
 	assert.NotNil(suite.T(), newBid)
 
-	existingBid := suite.repository.FindBidByItem(item, suite.actionUser)
+	existingBid := suite.repository.FindBidByItem(item, bidUser)
 	assert.NotNil(suite.T(), existingBid)
 	assert.True(suite.T(), existingBid.ID == newBid.ID)
 	assert.True(suite.T(), existingBid.Value == newBid.Value)
@@ -29,19 +36,26 @@ func (suite *ServiceTestSuite) TestPlaceBidOnItem() {
 
 func (suite *ServiceTestSuite) TestItDoesntPlaceBidOnItemAfterLastBidDate() {
 	suite.DB.Exec(`
+INSERT INTO users(id, uuid, created_at, updated_at, first_name, last_name, email, password, is_active) 
+VALUES (6, uuid_v4(), NOW(), NOW(), "John", "Doe", "johndoe25@abc.com", "$2a$10$3QxDjD1ylgPnRgQLhBrTaeqdsNaLxkk7gpdsFGUheGU2k.l.5OIf6", 1)
+`)
+	suite.DB.Exec(`
 UPDATE items SET last_bid_date = "2022-02-01" WHERE id = 2;
 `)
 	item := suite.repository.FindItemById(2)
 	assert.NotNil(suite.T(), item)
+	bidUser := suite.repository.FindUserById(6)
+	assert.NotNil(suite.T(), bidUser)
+	assert.False(suite.T(), item.IsOwner(*bidUser))
 
 	assert.True(suite.T(), item.LastBidDate.Format(time.RFC3339) == "2022-02-01T00:00:00Z")
 	assert.True(suite.T(), time.Now().After(item.LastBidDate))
 
 	placeBidForm := forms.PlaceNewItemBidForm{
 		ItemId:        item.ID,
-		BidUserId:     suite.actionUser.ID,
+		BidUserId:     bidUser.ID,
 		BidValue:      14,
-		AuditableForm: forms.AuditableForm{ActionUser: suite.actionUser},
+		AuditableForm: forms.AuditableForm{ActionUser: bidUser},
 	}
 	newBid, err := suite.itemService.PlaceItemBid(context.TODO(), placeBidForm)
 	assert.Nil(suite.T(), newBid)
