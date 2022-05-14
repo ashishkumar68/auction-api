d0ab3b2d4cafed70eb460e94bed0bd338737c6fb
Ashish Kumar <ashish.k@mindfiresolutions.com>
2022-05-14 20:29:53 +0530

    Added graceful shutdown capability.

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/ashishkumar68/auction-api/config"
 	"github.com/ashishkumar68/auction-api/middleware"
@@ -9,7 +10,11 @@ import (
 	"github.com/ashishkumar68/auction-api/validators"
 	"github.com/gin-gonic/gin"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func SetupRoutes(engine *gin.Engine) {
@@ -34,6 +39,14 @@ func main() {
 	// setting up custom validators.
 	validators.SetupCustomValidators()
 
+	if config.AppEnvTest == os.Getenv("APP_ENV") {
+		launchDevelopmentServer(engine)
+	} else {
+		launchReleaseServer(engine)
+	}
+}
+
+func launchDevelopmentServer(engine *gin.Engine) {
 	// launch server.
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -44,4 +57,45 @@ func main() {
 		log.Fatalln("could not start server", err)
 	}
 	log.Println(fmt.Sprintf("server listening on port: %s", port))
+}
+
+func launchReleaseServer(engine *gin.Engine) {
+	// launch server.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8081"
+	}
+
+	port = fmt.Sprintf(":%s", port)
+	server := &http.Server{
+		Addr:    port,
+		Handler: engine,
+	}
+
+	go func() {
+		log.Println(fmt.Sprintf("server listening on port: %s", port))
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatalln("could not start server", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall. SIGKILL but can't be caught, so don't need to add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Initiating Shutdown Server ...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal("Error while Server Shutdown:", err)
+	}
+	// catching ctx.Done(). timeout of 5 seconds.
+	select {
+	case <-ctx.Done():
+		log.Println("timeout of 5 seconds.")
+	}
+	log.Println("Server gracefully shutdown.")
 }
