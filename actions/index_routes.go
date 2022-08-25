f2323c1f3bc6138de8a0fd08331f21dfe1bf4062
Ashish Kumar <ashish.k@mindfiresolutions.com>
2022-08-25 13:45:17 +0530

    Added a new Version endpoint.

--- actions/index_routes.go
+++ actions/index_routes.go
@@ -11,4 +11,6 @@ const (
 func MapIndexRoutes(engine *gin.Engine) {
 	engine.GET("/", CorsRoute(), IndexAction)
 	engine.OPTIONS("/", CorsRoute())
+
+	engine.GET("/version", FetchBuildVersionAction)
 }
